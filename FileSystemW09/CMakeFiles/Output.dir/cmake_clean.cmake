5a96bbfdac888a666a8f0669407a0c3e81f6099c
Thanh Nguyen <thanhqng1510@gmail.com>
2019-07-23 11:10:13 +0700

    update

--- FileSystemW09/CMakeFiles/Output.dir/cmake_clean.cmake
+++ FileSystemW09/CMakeFiles/Output.dir/cmake_clean.cmake
@@ -3,8 +3,8 @@ file(REMOVE_RECURSE
   "CMakeFiles/Output.dir/src/CFolder.cpp.o"
   "CMakeFiles/Output.dir/src/CItem.cpp.o"
   "CMakeFiles/Output.dir/src/Main.cpp.o"
-  "Output"
   "Output.pdb"
+  "Output"
 )
 
 # Per-language clean rules from dependency scanning.
