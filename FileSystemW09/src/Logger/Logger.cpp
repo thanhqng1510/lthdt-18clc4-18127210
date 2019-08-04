17b813db5d0aa7e55e903c7f12edeae9cfe010c9
ThanhNguyen <thanhqng@ThanhNguyens-MacBook-Pro.local>
2019-08-04 23:58:50 +0700

    update

--- FileSystemW09/src/Logger/Logger.cpp
+++ FileSystemW09/src/Logger/Logger.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
+
 #include "Logger.hpp"
 
-Logger& Logger::GetLogger() {
+Logger& Logger::GetInstance() {
     static Logger logger;
     return logger;
+}
+
+void Logger::Log(LOG_LEVEL level, LOG_DISPLAY display, const std::string& class_name, const std::string& func_name) {
+    if (display == LOG_DISPLAY::CONSOLE) {
+         
+    }
+    else {
+
+    }
 }
--- FileSystemW09/src/Logger/Logger.hpp
+++ FileSystemW09/src/Logger/Logger.hpp
@@ -1,11 +1,34 @@
 #pragma once
 
+#include <string>
+
+enum class LOG_LEVEL {
+    INFO,
+    DEBUG,
+    WARNING, 
+    ERROR,
+    FUNCTIONAL_MESSAGE, 
+    FUNCTIONAL_ERROR
+};
+
+enum class LOG_DISPLAY {
+    CONSOLE, 
+    FILE
+};
+
 class Logger {
 public:
-    static Logger& GetLogger();
+    static Logger& GetInstance();
+
+private:
+    Logger() = default;
 
 public:
-    Logger() = delete;
     Logger(const Logger& logger) = delete;
+    Logger(const Logger&& logger) = delete;
     Logger& operator = (const Logger& other) = delete;
+    Logger& operator = (const Logger&& other) = delete;
+
+public:
+    void Log(LOG_LEVEL level, LOG_DISPLAY display, const std::string& class_name, const std::string& func_name);
 };
